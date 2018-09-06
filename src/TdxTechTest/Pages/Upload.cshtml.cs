26a5cf9487c516185a1cce90d1cc527ae481e93c
Simon <sbridge67@gmail.com>
2018-09-06 18:26:08 +0100

    Add upload view

---
+++ src/TdxTechTest/Pages/Upload.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TdxTechTest.Pages
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
